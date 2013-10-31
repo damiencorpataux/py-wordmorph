77b352fe604ba1b309b2a695d9b7d91be4962aa3
Damien Corpataux <d@mien.ch>
2013-10-31 16:34:16 +0100

    Recursive morph algorith trial: works but.

--- morph.py
+++ morph.py
@@ -12,7 +12,7 @@ cache = {
 
 # FIXME: Proposal: yield data
 
-def words():
+def allwords():
     with open(options.get('wordlist')) as f:
         cache['words'] = f.read().splitlines()
     return cache.get('words')
@@ -31,14 +31,29 @@ def neighbourhood(word, words, max_distance=1):
         #        And create a dedicated distance()
         distance = 0
         for i, l in enumerate(candidate):
-            if candidate[i] != word[i]: distance += 1
             if distance > max_distance: continue
+            if candidate[i] != word[i]: distance += 1
         if distance <= max_distance:
             neighbours.append(candidate)
     return neighbours
 
-def next():
-    pass
+def fu(source, target, maxdepth=10, current=[], paths=[]):
+    if not current: current = [source]
+    words = allwords()
+    words = set(words) - set(current)
+    for word in neighbourhood(current[-1], words):
+        current.append(word)
+        if (word == target):
+            print "FOUND:", len(current), current
+            paths.append(current)
+            current = current[:-1]
+        #elif len(current) < maxdepth:
+            print len(current), maxdepth
+            return fu(current[-1], target, maxdepth, current, paths)
+    return paths
 
 if __name__ == '__main__':
-    print neighbourhood('cast', words(), 2)
+    paths = fu('cast', 'cash', 10)#'hurt')
+    print 'RESULT:'
+    for path in paths:
+        print len(path), path
